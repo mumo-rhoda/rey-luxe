49a1d431bdbd1764f0befcdd9c14b3c03f4b5c8d
mumo-rhoda <rhodamumo117@gmail.com>
2025-01-26 17:10:51 +0300

    init stuct

---
+++ src/main/java/rey/cosmetics/luxe/LuxeApplication.java
@@ -0,0 +1,13 @@
+package rey.cosmetics.luxe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuxeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuxeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rey/cosmetics/luxe/LuxeApplicationTests.java
@@ -0,0 +1,13 @@
+package rey.cosmetics.luxe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuxeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
